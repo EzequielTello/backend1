94dddb93d60c22a503012ed216776435d6c31b0c
Tu Nombre <ezetello23@gmail.com>
2023-12-28 19:39:57 -0300

    aggregando metodos

--- entregable2.js
+++ entregable2.js
@@ -28,6 +28,32 @@ class ProductManager {
     return this.products;
   }
 
+  getProductById(id) {
+    const product = this.products.find((p) => p.id === id);
+
+    if (product) {
+      return product;
+    } else {
+      console.log(`Producto con ID ${id} no encontrado (NOT FOUND)`);
+      return null;
+    }
+  }
+
+  updateProduct(id, updatedProduct) {
+    const productIndex = this.products.findIndex((p) => p.id === id);
+
+    if (productIndex !== -1) {
+      this.products[productIndex] = {
+        ...this.products[productIndex],
+        ...updatedProduct,
+      };
+      this.saveProducts();
+      console.log(`Producto con ID ${id} actualizado correctamente`);
+    } else {
+      console.log(`No existe ningún producto con el ID ${id}`);
+    }
+  }
+
   addProduct(title, description, price, code, stock) {
     if (!title || !description || !price || !code || !stock) {
       console.log("Todos los campos son obligatorios");
@@ -72,10 +98,23 @@ console.log("---------");
 console.log(productManager.getProducts());
 
 console.log("---------");
-productManager.addProduct("Toyota Hilux ", "4x4 srx", 5000, "hh", 12);
+productManager.addProduct("Toyota Corolla ", "sedan 1.8 xei", 3000, "cc", 10);
 
 console.log("---------");
 console.log(productManager.getProducts());
+
+console.log("---------");
+const productId = productManager.getProducts()[0].id;
+console.log(productManager.getProductById(productId));
+
+console.log("---------");
+productManager.updateProduct(productId, {
+  title: "Nuevo Toyota Hilux",
+  description: "4x4 3.2 full",
+  price: 6000,
+  code: "nhh",
+  stock: 15,
+});
 
 console.log("---------");
 productManager.deleteProduct(productManager.getProducts()[0].id);
